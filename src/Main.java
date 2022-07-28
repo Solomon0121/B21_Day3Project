199870fd403863df8f1d433db714b84afb7489ca
Solomon0121 <solomonmoon01@gmail.com>
2022-07-28 13:40:18 -1000

    working on nice message

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,7 @@ public class Main {
     public static void main(String[] args) {
             // write your code here
             System.out.println("Hello Git Last Day");
-
-
+            // xxxxxxxxx
     }
 
 }
